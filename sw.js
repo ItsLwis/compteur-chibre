a5e6528880b0c1704b39e3179eac12924a7af370
Lwis <161162343+ItsLwis@users.noreply.github.com>
2025-09-08 21:03:30 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE='cc-v8-1-cache';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icons/app-192.png','./icons/app-512.png'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
